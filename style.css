3233f881379106a69136a6dac13e67fe65d037da
Rohit2829 <119347115+Rohit2829@users.noreply.github.com>
2024-07-03 22:32:10 +0530

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,57 @@
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
+*{
+    margin: 0;
+    padding:0;
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+}
+body{
+    width: 100%;
+    height: 90%;
+    display: flex;
+    justify-content:center ;
+    align-items: center;
+    background: linear-gradient(45deg,#332667,#0a000b);
+}
+.calcii{
+    border: 1px solid #717377;
+    padding: 20px;
+    border-radius: 16px;
+    background: transparent;
+    box-shadow: 0px 3px 15px rgba(62, 71, 168, 0.5) ;
 
+}
+input{
+    width: 320px;
+    border:none;
+    padding: 24px;
+    margin: 10px;
+    background: rgb(118, 116, 117);
+    box-shadow: 0px 3px 15px rgba(45, 95, 152, 0.6);
+    font-size: 40px;
+    text-align: right;
+    cursor: pointer;
+    color: #ffffff;
+}
+input::placeholder{
+    color: white;
+}
+button{
+    border:none ;
+    width: 60px;
+    height: 60px;
+    margin: 10px;
+    border-radius: 50%;
+    background: transparent;
+    color: #ffffff;
+    font-size: 20px;
+    box-shadow: -8px -8px 15px rgba(84, 101, 139, 0.2) ;
+    cursor:pointer;
+    transform: scale(0.99);
+}
+.equalbtn{
+    background-color: rgb(234, 109, 64);
+}
+.operator{
+    color:rgba(57, 255, 255, 0.669) ;
+}
